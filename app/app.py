db511d7eac2dc5d88ae53b7e3687d2e4125152b7
stephiioo <stephihiioo@gmail.com>
2023-12-16 23:50:44 +0000

    added 0auth

--- app/app.py
+++ app/app.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, url_for, redirect, session
+from authlib.integrations.flask_client import OAuth
+from authlib.common.security import generate_token
 from database import Base, Patient, Doctor, MedicalRecord
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 import logging
+from db_functions import update_or_create_user
 
 load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = os.urandom(12)
+oauth = OAuth(app)
 
 ## Database configuration
 DATABASE_URL = "sqlite:///local.db"
@@ -64,5 +69,47 @@ def get_sample_data():
     logging.debug("API endpoint criteria fulfilled")
     return jsonify(sample_data)
 
+# Google OAuth
+GOOGLE_CLIENT_ID = '437210046789-b0fru5oi02e3svmnroigp84jbhvjhf02.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = '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'
+
+@app.route('/google/')
+def google():
+    CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+    oauth.register(
+        name='google',
+        client_id=GOOGLE_CLIENT_ID,
+        client_secret=GOOGLE_CLIENT_SECRET,
+        server_metadata_url=CONF_URL,
+        client_kwargs={
+            'scope': 'openid email profile'
+        }
+    )
+    redirect_uri = url_for('google_auth', _external=True)
+    session['nonce'] = generate_token()
+    return oauth.google.authorize_redirect(redirect_uri, nonce=session['nonce'])
+
+@app.route('/google/auth/')
+def google_auth():
+    token = oauth.google.authorize_access_token()
+    user = oauth.google.parse_id_token(token, nonce=session['nonce'])
+    session['user'] = user
+    update_or_create_user(user)
+    print(" Google User ", user)
+    return redirect('/dashboard')
+
+@app.route('/dashboard/')
+def dashboard():
+    user = session.get('user')
+    if user:
+        return render_template('dashboard.html', user=user)
+    else:
+        return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
